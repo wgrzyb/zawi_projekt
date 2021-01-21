3be43ff7476359113b99640056e70c99f5b434a7
karbar57 <karol.baranowski@student.wat.edu.pl>
2021-01-21 14:17:48 +0100

    owl update

--- flask_app/ontology.py
+++ flask_app/ontology.py
@@ -1,4 +1,4 @@
 from owlready2 import *
-onto = get_ontology("AtlasZwierzat.owl").load()
+onto = get_ontology("Atlas_Zwierzat.owl").load()
 print(list(onto.classes()))
 print(list(onto.object_properties()))
