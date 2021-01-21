271c2b4710bf9206ee52b244293e29e987ae9580
karbar57 <karol.baranowski@student.wat.edu.pl>
2021-01-21 15:56:58 +0100

    basic reasoner

--- flask_app/ontology.py
+++ flask_app/ontology.py
@@ -2,3 +2,11 @@ from owlready2 import *
 onto = get_ontology("Atlas_Zwierzat.owl").load()
 print(list(onto.classes()))
 print(list(onto.object_properties()))
+print(list(onto.data_properties()))
+
+
+with onto:
+    sync_reasoner_pellet() # Wnioskowanie Pelletem
+
+
+
