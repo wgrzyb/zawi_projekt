98607b7df54af95a62d369fd7b73a4c37ad1c221
wgrzyb <wgrzyb99@gmail.com>
2021-01-21 20:17:46 +0100

    Zaimplementowano formularz do wyszukiwania gatunków

--- flask_app/main.py
+++ flask_app/main.py
@@ -13,12 +13,27 @@ def home():
     return render_template('home.html', title='Strona główna')
 
 
+def get_features():
+    features = {"Środowisko życia": {"id": "living_environment",
+                                     "type": "str"},
+                "Liczba nóg": {"id": "n_legs",
+                               "type": "int"},
+                "Czy posiada skrzydła?": {"id": "has_wing",
+                                          "type": "bool"},
+                "Czy potrafi pływać?": {"id": "can_swim",
+                                        "type": "bool"}
+                }
+    return features
+
+
 @app.route('/find_species', methods=['GET', 'POST'])
 def find_species():
     if request.method == 'POST':
+        for feature in request.form:
+            print(f"{feature}={request.form.get(feature)}")
         flash(f"Wiadomość!", 'alert alert-success')
         return redirect(request.url)
-    return render_template('find_species.html', title='Znajdź gatunek')
+    return render_template('find_species.html', title='Znajdź gatunek', features=get_features())
 
 
 if __name__ == '__main__':
